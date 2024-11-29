19fe75b5f70e5c6912e11d8fe09441b068b81aa1
HATBE <admin@aarongensetter.ch>
2024-11-29 16:56:02 +0100

    javadoc

--- backend/src/main/java/ch/hatbe/soeproject/utils/BookingUtil.java
+++ backend/src/main/java/ch/hatbe/soeproject/utils/BookingUtil.java
@@ -6,6 +6,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class BookingUtil {
+    /**
+     * @param bookings a list of bookings
+     * @param startDate start date of a booking
+     * @param endDate end date of a booking
+     * @return returns true if one of the bookings intersects with the range (startDate, endDate)
+     */
     public static boolean doBookingsOverlap(List<Booking> bookings, LocalDate startDate, LocalDate endDate) {
         List<Booking> overlappingBookings = bookings.stream()
                 .filter(b -> !(b.getEndDate().isBefore(startDate) || b.getStartDate().isAfter(endDate)))
